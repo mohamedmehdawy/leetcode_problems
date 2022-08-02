3e45e07395e629b1dfabc805da819bec0785bf06
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2022-08-02 23:06:01 +0200

    add some retouch in solution

--- Longest Substring Without Repeating Characters/master.py
+++ Longest Substring Without Repeating Characters/master.py
@@ -1,32 +1,17 @@
 class Solution:
-    def __init__(self):
-        self.max_sub = {
-            "sub": "",
-            "length": 0
-        }
-        self.current = ""
-    def setMaxSub(self) -> None:
-        self.max_sub["sub"] = self.current
-        self.max_sub["length"] = len(self.current)
-        
     def lengthOfLongestSubstring(self, s: str) -> int:
+        max = 0
+        current = ""
         for i in range(len(s)):
-            self.current = s[i]
-            if len(self.current) > self.max_sub["length"]:
-                self.setMaxSub()
+            current = s[i]
             for j in range(i + 1, len(s)):
-                if s[j] not in self.current:
-                    self.current += s[j]
-                    
+                if s[j] not in current:
+                    current += s[j]
                 else:
-                    if len(self.current) > self.max_sub["length"]:
-                        self.setMaxSub()
                     break
-                if len(self.current) > self.max_sub["length"]:
-                    self.setMaxSub()
-
-
-        return self.max_sub["length"]
+            if len(current) > max:
+                max = len(current)           
+        return max
         
         
 obj = Solution()
