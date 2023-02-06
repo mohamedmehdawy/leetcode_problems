2b6a9339d778c8a73a953e9544578fdea5eace61
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-02-06 13:17:53 +0200

    decreasing time complexity by using stack

--- Valid Parentheses/master.py
+++ Valid Parentheses/master.py
@@ -16,57 +16,25 @@ class Solution:
             return side.index(value) + 1
         except:
             return False
-    def isValid(self, s: str) -> bool: # O(2^n)
-        size = len(s)
-        # length of s is odd return false
-        if size % 2 != 0:
+    def isValid(self, s: str) -> bool: # O(n)
+        if len(s) % 2 != 0:
             return False
-        
         else:
-            i = 0
-            expected = size // 2 # the expected number of paranthes
-            while i <= size - 2:
-                open_pos = self.getPos(self.left, s[i])
-                if open_pos:
-                    
-                    if self.getPos(self.left, s[i]) == self.getPos(self.right, s[i+1]):
-                        i = i + 2
-                        expected -= 1
-
-                    else:
-                        count = 1
-                        is_valid = False
-                        scope = {
-                        "start": -1,
-                        "end": -1
-                    }
-                        for j in range(i+1, size): # check if paranthe is valid
-                            if s[i] == s[j]:
-                                count += 1
-                            elif self.getPos(self.left, s[i]) == self.getPos(self.right, s[j]):
-                                if count == 1:
-                                    is_valid = True
-                                    expected -= 1
-                                    scope['start'] = i
-                                    scope['end'] = j
-                                    break
-                                else:
-                                    count -= 1
-                        if is_valid:
-                            status = self.isValid(s[scope['start']+1:scope['end']])
-                            if status:
-                                i = scope['end'] + 1
-                            else:
-                                return False
-                        else:
-                            return False
-                    
-                    # check if expected number of paranthes end
-                    if expected == 0:
-                        return True
-                else: 
+            # create stack to store paranthese in it
+            stack = []
+            
+            for char in s:
+                if self.getPos(self.left, char): # if current char in left side append it to stack
+                    stack.append(char)
+                elif len(stack) and self.getPos(self.right, char) == self.getPos(self.left, stack[-1]): # if current char in right side and the last element in stack is the open of it remove last element from stack and the stack not empty
+                    stack.pop()
+                else:
                     return False
-            return True
+            # return true if stack is empty
+            if not len(stack):
+                return True
+            else:
+                return False
         
 obj = Solution()
 
