f7dde339e4a2951e994c07fd6ee3627e49d41cd5
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-02-21 07:23:00 +0200

    add time complexity for Remove Duplicates from Sorted Array

--- Remove Duplicates from Sorted Array/master.py
+++ Remove Duplicates from Sorted Array/master.py
@@ -1,5 +1,5 @@
 class Solution:
-    def removeDuplicates(self, nums: list[int]) -> int:
+    def removeDuplicates(self, nums: list[int]) -> int: # O(n)
         # set valid area to 1 beacause we have at least 1 valid number
         valid_area = 1
         for i in range(1, len(nums)):
