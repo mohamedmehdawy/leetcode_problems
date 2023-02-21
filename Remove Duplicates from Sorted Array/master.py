6703987ae101eba4c567202fe6dfba52e3af9c37
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-02-21 07:14:21 +0200

    improve my solution for Remove Duplicates from Sorted Array

--- Remove Duplicates from Sorted Array/master.py
+++ Remove Duplicates from Sorted Array/master.py
@@ -1,32 +1,12 @@
 class Solution:
     def removeDuplicates(self, nums: list[int]) -> int:
-        
-        # area of valid numbers
-        valid_area = len(nums) - 1
-        
-        # set i and j
-        i = 0
-        j = i + 1
-        
-        while i <= valid_area:
-            j = i + 1
-            while j <= valid_area:
-                # if current element found in another place replace, swap another place in the end of valid area and decrace valid area by 1
-                if nums[i] == nums[j]:
-                    nums[j], nums[valid_area] = nums[valid_area], nums[j]
-                    valid_area -= 1
-                else:
-                    j += 1
-            i += 1
-        
-        # sort valid area
-        slice_area = nums[:valid_area+1]
-        slice_area.sort()
-        
-        for i in range(valid_area+1):
-            nums[i] = slice_area[i]
-        
-        return valid_area + 1
-        
+        # set valid area to 1 beacause we have at least 1 valid number
+        valid_area = 1
+        for i in range(1, len(nums)):
+            # if we found number not eqaul last number in valid area, put this number in the last of our valid area and increase it by one
+            if nums[valid_area - 1] != nums[i]:
+                nums[valid_area] = nums[i]
+                valid_area += 1
+        return valid_area        
 obj = Solution()
-print(obj.removeDuplicates([1,1,1]))
+print(obj.removeDuplicates([1,1,1, 2, 2, 2, 3, 4,4]))
