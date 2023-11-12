539ad7e1e23d0eeff7fed3360a5e9bb9edb636fe
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-11-12 10:40:29 +0200

    imporve valid parentheses

--- Valid Parentheses/master.py
+++ Valid Parentheses/master.py
@@ -1,40 +1,45 @@
-class Solution:
+class Solution(object):
     def __init__(self):
-        self.left = ['(', '{', '[']
-        self.right = [')', '}', ']']
-    def getPos(self, side, value): # O(n)
+        self.open_symbols = ["[", "(", "{"]
+        self.close_symbols = ["]", ")", "}"]
+        
+    def get_position(self, currentType, char):
         """
-            this function return position of target in side
-            -- why not return index?
-            - because 0 work as False so we need return position not index
-            parametrs:
-                side: left or right
-                value: should be search
-            return: boolean value
+            this function return the position of char insted of the type is open or close
+            parameters:
+                currentType: use open or close elements
+                char: the char will search
+            returns:
+                the position or -1 if not found
         """
-        try:
-            return side.index(value) + 1
-        except:
-            return False
-    def isValid(self, s: str) -> bool: # O(n)
+        
+        return currentType.index(char) if char in currentType else -1
+
+    def isValid(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        # check if the length of the string is not have 2 pieces
         if len(s) % 2 != 0:
             return False
-        else:
-            # create stack to store paranthese in it
-            stack = []
-            
-            for char in s:
-                if self.getPos(self.left, char): # if current char in left side append it to stack
-                    stack.append(char)
-                elif len(stack) and self.getPos(self.right, char) == self.getPos(self.left, stack[-1]): # if current char in right side and the last element in stack is the open of it remove last element from stack and the stack not empty
-                    stack.pop()
-                else:
-                    return False
-            # return true if stack is empty
-            if not len(stack):
-                return True
+        
+        # create the stack
+        stack = []
+        
+        for char in s:
+            # if the current char is in open append it to the stack
+            if char in self.open_symbols:
+                stack.append(char)
+            # if the current char is close, should be the last element in stack is the open of it
+            elif stack and self.get_position(self.close_symbols, char) == self.get_position(self.open_symbols, stack[-1]):
+                stack.pop()
+            # return false because this is not valid
             else:
                 return False
+        
+        # if have elments in stack, this is valid and return false
+        return not stack
         
 obj = Solution()
 
