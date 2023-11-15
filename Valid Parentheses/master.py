686550855cdef3b50564d011d7fc072598b6ed67
Mohamed Mehdawy <mohammedmehdawy@gmail.com>
2023-11-15 06:02:21 +0200

    improve valid parentheses

--- Valid Parentheses/master.py
+++ Valid Parentheses/master.py
@@ -1,20 +1,4 @@
 class Solution(object):
-    def __init__(self):
-        self.open_symbols = ["[", "(", "{"]
-        self.close_symbols = ["]", ")", "}"]
-        
-    def get_position(self, currentType, char):
-        """
-            this function return the position of char insted of the type is open or close
-            parameters:
-                currentType: use open or close elements
-                char: the char will search
-            returns:
-                the position or -1 if not found
-        """
-        
-        return currentType.index(char) if char in currentType else -1
-
     def isValid(self, s):
         """
         :type s: str
@@ -25,24 +9,16 @@ class Solution(object):
             return False
         
         # create the stack
+        dct = {')': '(', '}':'{', ']':'['}
         stack = []
         
         for char in s:
             # if the current char is in open append it to the stack
-            if char in self.open_symbols:
+            if char not in dct:
                 stack.append(char)
-            # if the current char is close, should be the last element in stack is the open of it
-            elif stack and self.get_position(self.close_symbols, char) == self.get_position(self.open_symbols, stack[-1]):
-                stack.pop()
-            # return false because this is not valid
-            else:
+            # if its the close, should be match with the open
+            elif not stack or dct[char] != stack.pop():
                 return False
         
         # if have elments in stack, this is valid and return false
         return not stack
-        
-obj = Solution()
-
-# print(obj.isValid('()'))
-print(obj.isValid('}{'))
-print(obj.isValid('[{}{}{}{}{}(([[]]))]'))
